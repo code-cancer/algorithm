d9a68150a61c0f493cb92e6c8cab78251162789d
zhifu <zhifu.zhang@ihandysoft.cn>
2023-09-28 19:05:07 +0800

    add sort code

---
+++ Common/PrintHelper.java
@@ -0,0 +1,18 @@
+package Common;
+
+public class PrintHelper {
+    
+    public static String toString(int[] arr) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("[");
+        for (int i = 0; i < arr.length; i++) {
+            builder.append(arr[i]);
+            if(i < arr.length - 1) {
+                builder.append(",");
+            }
+        }
+        builder.append("]");
+        return builder.toString();
+    }
+
+}
